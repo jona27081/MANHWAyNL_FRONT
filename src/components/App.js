0457aa063d1fcc0c694d435829c7bd083d828bdb
OJEDA ROSAS JONATHAN ARATH <zs20006736@estudiantes.uv.mx>
2023-05-02 00:47:13 -0500

    add function created manhwa

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import LinkList from './LinkList';
 import './../styles/App.css';
 import Header from './Header';
 import { Route, Routes } from 'react-router-dom';
+import CreateLink from './CreateLink';
+import Profile from './About';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <div className="">
         <Routes>
           <Route path="/manhwas" element={<LinkList/>} />
+          <Route path="/subir" element={<CreateLink/>} />
+          <Route path="/about" element={<Profile/>} />
         </Routes>
       </div>
     </div>
