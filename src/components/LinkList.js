ad6b58754c7567930e55150fff0c719391c6d049
OJEDA ROSAS JONATHAN ARATH <zs20006736@estudiantes.uv.mx>
2023-05-01 21:55:14 -0500

    init repo

---
+++ src/components/LinkList.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Link from './Link';
+import { useApolloClientq, gql, useQuery } from '@apollo/client';
+
+const FEED_QUERY = gql`
+query{
+  manhwas{
+    id,
+    titulo,
+    descripcion,
+    capitulos
+  }
+}
+`
+
+const LinkList = () => {
+  /*const linksToRender = [
+    {
+      id: 'link-id-1',
+      description:
+        'Prisma gives you a powerful database toolkit 😎',
+      url: 'https://prisma.io'
+    },
+    {
+      id: 'link-id-2',
+      description: 'The best GraphQL client',
+      url: 'https://www.apollographql.com/docs/react/'
+    }
+  ];
+
+  return (
+    <div>
+      {linksToRender.map((link) => (
+        <Link key={link.id} link={link} />
+      ))}
+    </div>
+  );*/
+
+  const { data } = useQuery(FEED_QUERY);
+
+  return (
+    <div>
+      {data && (
+        <>
+          {data.manhwas.map((link) => (
+            <Link key={link.id} link={link} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default LinkList;
